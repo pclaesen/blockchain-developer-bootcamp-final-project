5db8da7a8391ddc2966f93c4e9f321844de05c2a
pclaesen <pieterclaesen@gmail.com>
2021-10-20 19:00:09 +0200

    Add contract code

--- contracts/fraudBattle.sol
+++ contracts/fraudBattle.sol
@@ -4,4 +4,24 @@ pragma solidity >=0.4.22 <0.9.0;
 contract fraudBattle {
   constructor() public {
   }
+
+
+  struct Banks {
+    address _addressBank;
+    string _name;
+    
+
+  }
+
+  //create bank array
+  Banks banks;
+
+  function addUser(address _address, string memory _name) public {
+    banks = Banks(_address, _name);
+  }
+
+
+
+
+
 }
