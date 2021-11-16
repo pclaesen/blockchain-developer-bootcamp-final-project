527efd2da5d6869bbe37e1147f5cc359271701b5
pclaesen <42892965+pclaesen@users.noreply.github.com>
2021-11-16 17:00:51 +0100

    Added mappings, changed functions to write to mappings

--- contracts/fraudBattle.sol
+++ contracts/fraudBattle.sol
@@ -7,8 +7,14 @@ contract fraudBattle {
     // address government "0x1D119A6929B1FCBaC8BeA4c610998f9Dfcaf184d" public; //we'll only use 1 gov. address for the scope of this final project
   }
 
+  address private owner;
 
-  struct Banks {
+  mapping (address => bool) isBank;
+  mapping (address => bool) isBusiness;
+  mapping (uint => bool) isBankSigned;
+
+
+  struct Banks  {
     address _addressBank;
     string _name;
   }
@@ -18,38 +24,47 @@ contract fraudBattle {
     string _name;
     string _bankAccount;
     uint _companyNumber;
-    string _bankName;
+    string _bankName;    
   }
 
-  struct signedInformation {
-    uint _companyNumberSigned;
-    mapping (uint => uint) txSigned;
-    
-
-
+  struct validBusinesses {
+    address _addressBus;
+    string _name;
+    string _bankAccount;
+    uint _companyNumber;
+    string _bankName;   
   }
+
+ 
 
   
   
   Banks[] private bankArray;
   Businesses[] private businessesArray;
+  validBusinesses[] public validBusinessesArray;
+  
 
   modifier bankOnly(address _bankAddress) {
-    for (uint i=0; i < bankArray.length; i++) {
-      require (bankArray[i]._addressBank == _bankAddress);
-        revert();
+    require (isBank[msg.sender],"Not a bank");
       _;
     }
+  
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+      _;
   }
 
-  function addBankUser(address _address, string memory _registeredName) public {    
+  function addBankUser(address _address, string memory _registeredName) onlyOwner public {    
     Banks memory banksData = Banks(_address, _registeredName);
     bankArray.push(banksData);
+    isBank[_address] = true;
   }
 
   function addBusiness(address _address, string memory _name, string memory _bankAccount, uint _companyNumber, string memory _bankName) public {
     Businesses memory busData = Businesses(_address, _name, _bankAccount, _companyNumber, _bankName);
     businessesArray.push(busData);
+    isBusiness[_address] = true;
   }
 
   //3 parties need to sign a transaction to add the bank account and business name to a new array.
@@ -59,20 +74,14 @@ contract fraudBattle {
     uint businessesLength = businessesArray.length;
     for (uint i = 0; i < businessesLength; i++) {
       if (_providedCompanyNumber == businessesArray[i]._companyNumber) {
-
+        validBusinesses memory validBusData = validBusinesses(businessesArray[i]._addressBus, businessesArray[i]._name, businessesArray[i]._bankAccount, businessesArray[i]._companyNumber, businessesArray[i]._bankName);
+        validBusinessesArray.push(validBusData);
+        isBankSigned[businessesArray[i]._companyNumber] = true;
       }
 
-
-      
-      // if (_vatNumber == businessesArray[i]._vatNumber) {
-
-      // }
     }
-
 
   }
 
-
-
-
+//closing brace end of contract
 }
