a9d034b25b315f0f8e256390c06aa06a821f8dd2
pclaesen <pieterclaesen@gmail.com>
2021-12-20 08:03:51 +0100

    Bump compiler version to 0.8.9

--- contracts/fraudBattle.sol
+++ contracts/fraudBattle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity 0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
